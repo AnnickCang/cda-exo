9bbf0b7c6473b6da63a9a1ca9f99d4dbb6103bba
Annick <annick.cang@gmail.com>
2026-09-16 17:04:22 +0200

    feat(exo): add FizzBuzz exercice in Python

---
+++ exercices/algo/algorithmes.py
@@ -0,0 +1,19 @@
+def fizzBuzz(n:int):
+  reste_division_3 = n % 3
+  reste_division_5 = n % 5
+
+  if reste_division_3 == 0 and reste_division_5 == 0:
+    print("FizzBuzz")
+  elif reste_division_5 == 0:
+    print("Buzz")
+  elif reste_division_3 == 0:
+    print("Fizz")
+  else:
+    print(n)
+
+
+if __name__ == "__main__":
+  fizzBuzz(1)
+  fizzBuzz(6)
+  fizzBuzz(15)
+  fizzBuzz(20)
