3a27cf2de2c9b12225d5a800e03adab6f513a9dd
Annick <annick.cang@gmail.com>
2026-09-17 09:47:51 +0200

    feat(exo): add Palindrome exercise

--- exercices/algo/algorithmes.py
+++ exercices/algo/algorithmes.py
@@ -1,4 +1,4 @@
-def fizzBuzz(n:int):
+def fizzBuzz(n:int)-> None:
   reste_division_3 = n % 3
   reste_division_5 = n % 5
 
@@ -13,10 +13,50 @@ def fizzBuzz(n:int):
 
 
 def isPalindrome(chaine:str) -> bool:
-  chaine_normalise
+  chaine_normalise = ""
+  for c in chaine:
+    match c:
+      case " ":
+        continue
+      case "à" | "â":
+        chaine_normalise += "a"
+      case "é" | "è" | "ê" | "ë":
+        chaine_normalise += "e"
+      case "i" | "î" | "ï":
+        chaine_normalise += "i"
+      case "ô" | "ö":
+        chaine_normalise += "o"
+      case "û" | "ü":
+        chaine_normalise += "u"
+      case "ç":
+        chaine_normalise += "c"
+      case _:
+        chaine_normalise += c
+
+  taille_chaine = len(chaine_normalise)
+  for i in range(taille_chaine//2):
+    if chaine_normalise[i] == chaine_normalise[taille_chaine-i-1]:
+      continue
+    else:
+      return False
+
+  return True
 
 if __name__ == "__main__":
-  fizzBuzz(1)
-  fizzBuzz(6)
-  fizzBuzz(15)
-  fizzBuzz(20)
+  print("\nExercice 3.1 - FizzBuzz")
+  liste_nombre = [1, 6, 5, 15, 20]
+  for nombre in liste_nombre:
+    fizzBuzz(nombre)
+
+  print("\nExercice 3.2 - Palindrome")
+  liste_de_chaines = [
+    "élu par cette crapule",
+    "radar",
+    "kayak",
+    "coucou"
+  ]
+  for chaine in liste_de_chaines:
+    if isPalindrome(chaine):
+      print(f"'{chaine}' est un palindrome")
+    else:
+      print(f"'{chaine}' n'est pas un palindrome")
