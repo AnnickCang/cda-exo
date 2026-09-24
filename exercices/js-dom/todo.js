23758582d4cd4009a5773970090690b033b615e9
Annick <annick.cang@gmail.com>
2026-09-24 08:54:32 +0200

    chore(folders): add, rename and sort folders

---
+++ exercices/js-dom/todo.js
@@ -0,0 +1,13 @@
+let bouton = document.querySelector("button");
+/*let label = document.querySelector("input");
+let toDoList = document.querySelector("ul");
+const nouveauLi = document.toDoList.createElement("li");*/
+bouton.addEventListener("click", addToDo);
+//console.log("coucou");
+function addToDo() {
+  console.log("dans la fonction addToDo");
+  /*bouton.label = "ok";
+  alert("Bouton cliqué");
+  nouveauLi.textContent = "Tache 1";
+  toDoList.appendChild(nouveauLi);*/
+}
